99acc477859c065e143c6d1a3d6ff89567f47a36
Aakarshan-Chauhan <54583758+Aakarshan-Chauhan@users.noreply.github.com>
2020-10-25 02:01:51 +0530

    Buffer replay works now

--- Simple Policy Gradients.py
+++ Simple Policy Gradients.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import gym
-import numpy 
+import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import matplotlib.pyplot as plt
@@ -17,69 +17,91 @@ def get_network(input_shape, output_shape, hidden_units):
 def play_one(env:gym.envs, model:tf.keras.Model):
 	done = False
 	obs = env.reset()
-
-	episode_obs = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
-	episode_actions = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
-	episode_rewards = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
+	episode_obs = []
+	episode_actions =[]
+	episode_rewards =[]
 
 	i = 0
 	while not done:
-		episode_obs = episode_obs.write(i, obs)
+		episode_obs.append(obs)
 		obs = tf.constant([obs], dtype=tf.float32)
 		
 		action_logits = model(obs)
 		action  = tf.random.categorical(action_logits, 1)[0, 0]
-		episode_actions = episode_actions.write(i, action)
 
+		episode_actions.append(action)
 
 		obs, reward, done, _ = env.step(action.numpy())
-		episode_rewards = episode_rewards.write(i, reward)
+		episode_rewards.append(reward)
 
 		if done:
 			
 			break
 		i+=1
 
-	episode_obs = episode_obs.stack()
-	episode_actions = episode_actions.stack()
-	episode_rewards = episode_rewards.stack()
 	return episode_actions, episode_rewards, episode_obs
 
 def get_returns(rewards, gamma=1):
-	returns = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
-	
-	ret = tf.constant(0.0)
-	for i in tf.range(rewards.shape[0]):
-		ret = tf.reduce_sum(rewards)
-		returns = returns.write(i, ret)
+	returns = []
 
-	returns = returns.stack()
+	ret = tf.constant(0.0)
+	for i in tf.range(len(rewards)):
+		ret = tf.reduce_sum(rewards)
+		returns.append(ret)
+
 	return returns
 
-def get_loss(actions, returns, obs):
+def get_loss(actions, weights, obs, model):
 	action_probs = model(obs)
-	log_actions = tf.reduce_sum(tf.math.log(actions))
-	loss = -1*tf.reduce_mean(log_actions * returns)
+
+	actions = tf.squeeze(tf.random.categorical(action_probs,1), axis=1)
+
+
+	action_masks = tf.one_hot(actions, num_actions)
+
+	log_actions = tf.reduce_sum(action_masks * tf.math.log(action_probs), axis = 1)
+
+	loss = -1*tf.reduce_mean(log_actions * weights)
+	
+
 	return loss
 
 
-def train(env, gamma, model, optimizer):
+def train(env, gamma, model, optimizer, batch_size=1000):
 
 	batch_returns = []
 	batch_obs = []
-	epoch_reward = 0.
+	batch_acts = []
+	batch_weights = []
+	batch_lens = []
+	
 
-	while len(batch_returns) < batch_size:
-		actions , rewards = play_one(env, model)
+	while len(batch_obs) < batch_size:
+		actions , rewards, obs = play_one(env, model)
 		returns = get_returns(rewards, gamma)
-		batch_returns += [returns]*len(rewards)
 
+		batch_obs += obs
+		batch_lens.append(len(returns))
+		batch_acts.append(actions)
+		returns = np.sum(rewards)
+
+		batch_returns.append(returns)
+		for i in range(len(rewards)):
+
+			batch_weights.append([returns])
+		
+	
+	batch_obs = np.array(batch_obs)
+	batch_weights = np.array(batch_weights)
+	batch_weights = np.reshape(batch_weights, newshape=(-1, 1))
+
+
+	batch_weights = tf.cast(batch_weights, tf.float32)
 	with tf.GradientTape() as tape:
-		losses = get_loss(actions, returns)
-
+		losses = get_loss(batch_acts, batch_weights, [batch_obs], model)
 	policy_grads = tape.gradient(losses, model.trainable_variables)
 	optimizer.apply_gradients(zip(policy_grads, model.trainable_variables))
-	return tf.reduce_sum(rewards)
+	return batch_returns
 
 
 if __name__ == "__main__":
@@ -92,13 +114,13 @@ if __name__ == "__main__":
 	network = get_network(obs_shape, num_actions, 128)
 
 	total = []
-	for i in tqdm.tqdm(range(5000)):
+	for i in tqdm.tqdm(range(1000)):
 		rewards = train(env, 1, network, optimizer)
 		
-		total.append(rewards)
+		total.append(np.mean(rewards))
 	
 
-	plt.xlabel("Episodes")
+	plt.xlabel("Epochs")
 	plt.ylabel("total reward per episode")
 	plt.plot(total)
 	plt.show()
