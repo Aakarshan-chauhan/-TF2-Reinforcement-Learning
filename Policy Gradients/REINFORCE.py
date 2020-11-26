4f9396568b10351136fd61c22ea77bb1ece2bd75
Aakarshan-Chauhan <54583758+Aakarshan-Chauhan@users.noreply.github.com>
2020-11-27 01:04:57 +0530

    Fully functional update

--- Policy Gradients/REINFORCE.py
+++ Policy Gradients/REINFORCE.py
@@ -1,16 +1,14 @@
 import gym
 import numpy as np
 import tensorflow as tf
-import tqdm
 import matplotlib.pyplot as plt
 def get_model():
 	model = tf.keras.models.Sequential([
-		tf.keras.layers.Dense(32, activation = 'tanh'),
-		tf.keras.layers.Dense(32, activation='tanh'),
-		tf.keras.layers.Dense(num_actions, activation = 'linear')
+		tf.keras.layers.Dense(64, activation='relu'),
+		tf.keras.layers.Dense(64, activation='relu'),
+		tf.keras.layers.Dense(num_actions, activation = 'softmax')
 		])
 	return model
-
 
 def get_action(state):
 	state = tf.reshape(state, (-1, len_states))
@@ -21,8 +19,7 @@ def get_action(state):
 	action = tf.reshape(action , ())
 	return action
 
-
-def get_returns(rewards):
+def get_returns(rewards, actions):
 	length = rewards.shape[0]
 
 	old_g = tf.constant(0.0, dtype=tf.float32)
@@ -33,7 +30,26 @@ def get_returns(rewards):
 		G = G.write(i, [g])
 		old_g = g
 	G = G.stack()
+
+	# Normalize
+	mean = np.mean(G)
+	std = np.std(G) 
+	if std <= 0:
+		std = 1
+	G = (G - mean) / std
+
+	G = tf.keras.backend.one_hot(actions, num_actions) * G
 	return G	
+
+def get_score(states, returns):
+	states = tf.reshape(states, (-1, len_states))
+	logits = model(states)
+	loss_pi = tf.reduce_mean(
+		returns * tf.math.log(tf.clip_by_value(logits,1e-10,1-1e-10)))
+
+	return -loss_pi
+
+
 
 def play_episode():
 	states = []
@@ -57,19 +73,12 @@ def play_episode():
 	actions = np.array(actions , dtype=np.int64)
 	return states, rewards, actions
 
-def get_score(states, actions, returns):
-	states = tf.reshape(states, (-1, len_states))
-	logits = model(states)
-	loss_pi = tf.reduce_mean(
-		returns * tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=actions))
-	return -loss_pi
-
 
 def update_model(states, rewards, actions):
 	
 	with tf.GradientTape() as tape:
-		returns = get_returns(rewards)
-		logpi = get_score(states, actions,returns)
+		returns = get_returns(rewards,actions)
+		logpi = get_score(states,returns)
 	grad = tape.gradient(logpi, model.trainable_variables)
 	optimizer.apply_gradients(zip(grad, model.trainable_variables))
 
@@ -79,23 +88,36 @@ def train():
 	return np.sum(rewards)
 
 if __name__ == "__main__":
+
 	env = gym.make("CartPole-v1")
 	len_states = len(env.reset())
 	num_actions = env.action_space.n
 
-	optimizer = tf.keras.optimizers.Adam()
+	optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005)
 	gamma = 0.99
 
-	rews = []
-	rs = []
 	model = get_model()
-	s = env.reset()
-	for i in range(500):
+
+	num_episodes = 1000
+	highreward = float("-inf")
+	rews = []
+	running_avg = []
+	for i in range(num_episodes):
 		
-		rews.append(train())
-		if i %10 == 0:
-			print(f"Episode: {i}, reward = {np.mean(rews)}")
-			rs.append(np.mean(rews))
-			rews = []
-	plt.plot(rs)
+		r = train()
+		highreward = (r if r > highreward else highreward)
+		rews.append(r)
+		if i > 100:
+			mean = np.mean(rews[i-100:i])
+		else:
+			mean = np.mean(rews)
+		print(f"\rEpisode: {i} / {num_episodes} || Highest reward = {highreward} || current average = {mean}", flush=True, end='')
+		
+		running_avg.append(mean)
+	plt.title("Rewards per episode")
+	plt.xlabel("Episodes")
+	plt.ylabel("Rewards")
+	plt.plot(rews, label="Rewards per episode")
+	plt.plot( running_avg, label="Running Average reward")
+	plt.legend()
 	plt.show()
