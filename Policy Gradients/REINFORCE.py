d9038befeeb1bc965dd5a0cc9604e5bb3a99d36e
Aakarshan-Chauhan <54583758+Aakarshan-Chauhan@users.noreply.github.com>
2020-12-17 09:40:38 +0530

    v1 update

--- Policy Gradients/REINFORCE.py
+++ Policy Gradients/REINFORCE.py
@@ -1,123 +1,156 @@
-import gym
-import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
-def get_model():
-	model = tf.keras.models.Sequential([
-		tf.keras.layers.Dense(64, activation='relu'),
-		tf.keras.layers.Dense(64, activation='relu'),
-		tf.keras.layers.Dense(num_actions, activation = 'softmax')
-		])
-	return model
+import numpy as np
+import gym
+import tqdm
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow_probability import distributions
 
-def get_action(state):
-	state = tf.reshape(state, (-1, len_states))
-	logits = model(state)
+class PolicyNetwork(Model):
+    def __init__(self, obs_dims, n_actions):
+        super(PolicyNetwork, self).__init__()
+        self.d1 = Dense(128, activation='relu', input_shape=(obs_dims, ))
+        self.d2 = Dense(128, activation='relu')
+        self.pi = Dense(n_actions, activation='softmax')
 
-	action =tf.squeeze(tf.random.categorical(logits, 1))
-
-	action = tf.reshape(action , ())
-	return action
-
-def get_returns(rewards, actions):
-	length = rewards.shape[0]
-
-	old_g = tf.constant(0.0, dtype=tf.float32)
-
-	G = tf.TensorArray(dtype=tf.float32, size=length, name="Returns")
-	for i in reversed(range(length)):
-		g = rewards[i] + gamma * old_g
-		G = G.write(i, [g])
-		old_g = g
-	G = G.stack()
-
-	# Normalize
-	mean = np.mean(G)
-	std = np.std(G) 
-	if std <= 0:
-		std = 1
-	G = (G - mean) / std
-
-	G = tf.keras.backend.one_hot(actions, num_actions) * G
-	return G	
-
-def get_score(states, returns):
-	states = tf.reshape(states, (-1, len_states))
-	logits = model(states)
-	loss_pi = tf.reduce_mean(
-		returns * tf.math.log(tf.clip_by_value(logits,1e-10,1-1e-10)))
-
-	return -loss_pi
+    def __call__(self, obs, training=None):
+        x = self.d1(obs)
+        x = self.d2(x)
+        x = self.pi(x)
+        return x
 
 
+class ReplayBuffer():
+    def __init__(self):
+        self.obsMem = []
+        self.nextobsMem = []
+        self.actionMem = []
+        self.rewardMem = []
+        self.doneMem = []
 
-def play_episode():
-	states = []
-	actions = []
-	rewards = []
-	done = False
+    def push(self, obs, obs_, action, reward, done):
+        self.obsMem.append(obs)
+        self.nextobsMem.append(obs_)
+        self.actionMem.append(action)
+        self.rewardMem.append(reward)
+        self.doneMem.append(done)
+    
+    def get_returns(self, gamma):
+        G = np.zeros(len(self.rewardMem), dtype=np.float32)
+        g = 0.
+        for i in reversed(range(len(self.rewardMem))):
+            g = self.rewardMem[i] + gamma*g
+            
+            G[i] = g 
 
-	s = env.reset()
-	stepcount = 0
-	while not done:
-		a = get_action(s).numpy()
-		stepcount +=1
-		actions.append(a)
-		states.append(s)
-		s,r,done,info = env.step(a)
-		
-		rewards.append(r)
+            if done:
+                g = 0.
+                G[i] = g 
+                
+        G = (G - np.mean(G)) / np.std(G)
+        return G
 
-	rewards = np.array(rewards, dtype=np.float32)
-	states = np.array(states, dtype=np.float32)
-	actions = np.array(actions , dtype=np.int64)
-	return states, rewards, actions
+            
+    def pull(self):
+        return (
+            np.array(self.obsMem,dtype=np.float32),
+            np.array(self.nextobsMem, dtype=np.float32),
+            np.array(self.actionMem, dtype=np.int64),
+            np.array(self.rewardMem, dtype=np.float32),
+            np.array(self.doneMem, dtype=np.float32)
+        )
+    
+    def reset(self):
+        self.__init__()
 
+class Agent():
+    def __init__(self, learning_rate, obs_dims, n_actions, gamma):
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.obs_dims = obs_dims
+        self.n_actions = n_actions
 
-def update_model(states, rewards, actions):
-	
-	with tf.GradientTape() as tape:
-		returns = get_returns(rewards,actions)
-		logpi = get_score(states,returns)
-	grad = tape.gradient(logpi, model.trainable_variables)
-	optimizer.apply_gradients(zip(grad, model.trainable_variables))
+        self.Replay = ReplayBuffer()
+        self.Policy = PolicyNetwork(self.obs_dims, self.n_actions)
+        self.optimizer = Adam(learning_rate=self.learning_rate)
+    
+    @tf.function
+    def get_action(self, obs):
+        obs = tf.reshape(obs, (-1, self.obs_dims))
+        probs = self.Policy(obs)
 
-def train():
-	states, rewards, actions = play_episode()
-	update_model(states, rewards, actions)
-	return np.sum(rewards)
+        probs = distributions.Categorical(probs=probs)
+        action = probs.sample()
+        return action
+
+    #@tf.function
+    def train(self):
+        obs , obs_, action, reward, done = self.Replay.pull()
+        returns = self.Replay.get_returns(self.gamma)
+        with tf.GradientTape() as tape:
+            loss = 0
+            for idx, (g, s) in enumerate(zip(returns, obs)):
+                s = tf.reshape(s, (-1, self.obs_dims))
+
+                probs = self.Policy(s)
+                action_probs = distributions.Categorical(probs = probs)
+                logprob = action_probs.log_prob(action[idx])
+                loss += -tf.squeeze(logprob)*g
+       
+        policyGradient = tape.gradient(loss, self.Policy.trainable_variables)
+        self.optimizer.apply_gradients(zip(policyGradient, self.Policy.trainable_variables))
 
 if __name__ == "__main__":
+    env = gym.make("CartPole-v0")
 
-	env = gym.make("CartPole-v1")
-	len_states = len(env.reset())
-	num_actions = env.action_space.n
+    n_actions = env.action_space.n
+    obs_dims = env.reset().shape[0]
+    gamma = 1
+    learning_rate=0.0001
 
-	optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005)
-	gamma = 0.99
+    BATCH_SIZE = 1000
+    NUM_UPDATES = 500
 
-	model = get_model()
+    agent = Agent(learning_rate=learning_rate, obs_dims=obs_dims, n_actions=n_actions, gamma=gamma)
 
-	num_episodes = 1000
-	highreward = float("-inf")
-	rews = []
-	running_avg = []
-	for i in range(num_episodes):
-		
-		r = train()
-		highreward = (r if r > highreward else highreward)
-		rews.append(r)
-		if i > 100:
-			mean = np.mean(rews[i-100:i])
-		else:
-			mean = np.mean(rews)
-		print(f"\rEpisode: {i} / {num_episodes} || Highest reward = {highreward} || current average = {mean}", flush=True, end='')
-		
-		running_avg.append(mean)
-	plt.title("Rewards per episode")
-	plt.xlabel("Episodes")
-	plt.ylabel("Rewards")
-	plt.plot(rews, label="Rewards per episode")
-	plt.plot( running_avg, label="Running Average reward")
-	plt.legend()
-	plt.show()
+    scores = []
+    avg_scores = []
+
+    with tqdm.trange(NUM_UPDATES) as t:
+        for update in t:
+            
+            score = 0
+            s = env.reset()
+            done = False
+            scores = []
+            while True:
+                a = agent.get_action(s).numpy()[0]
+                s_, r, done, info = env.step(a)
+                
+                score  += r
+
+                r = (-195 if done else r)
+                agent.Replay.push(s, s_, a, r, done)
+                s = s_
+                if done:
+                    scores.append(score)
+
+                    score = 0
+                    s = env.reset()
+                    done = False
+                    t.set_description(
+                            f"size = {len(agent.Replay.rewardMem)}"
+                        )
+                    if len(agent.Replay.rewardMem) > BATCH_SIZE:
+                        break
+
+            agent.train()
+            agent.Replay.reset()
+            t.set_postfix(
+                            avg_score = np.mean(scores),
+                            
+                        )
+            avg_score = np.mean(scores)
+            avg_scores.append(avg_score)
+            
