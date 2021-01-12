bc5841f852beaf53053a9975c90c4b078fdedb90
Aakarshan-Chauhan <54583758+Aakarshan-Chauhan@users.noreply.github.com>
2021-01-13 01:12:39 +0530

    update

--- Policy Gradients/A2C.py
+++ Policy Gradients/A2C.py
@@ -1,146 +1,130 @@
-import gym
-import tqdm
-import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
-from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
-from tensorflow_probability import distributions
-
+from tensorflow.keras.losses import MSE
+import tensorflow_probability as tfp
+import numpy as np
+import matplotlib.pyplot as plt
+import wandb
+import tqdm
+import gym
 
 class Actor(Model):
-	def __init__(self, input_dims, num_outputs):
+	def __init__(self, n_actions):
 		super(Actor, self).__init__()
-		self.d1 = Dense(1024, activation='relu', input_shape=(input_dims, ))
+		self.d1 = Dense(1024, activation='relu')
 		self.d2 = Dense(512, activation='relu')
-		self.pi = Dense(num_outputs, activation='softmax')
+		self.pi = Dense(n_actions, activation='softmax')
 
-	def __call__(self, obs, training=None):
-		x = self.d1(obs)
+	def __call__(self, inp):
+		x = self.d1(inp)
 		x = self.d2(x)
 		return self.pi(x)
 
 class Critic(Model):
-	def __init__(self, input_dims):
+	def __init__(self):
 		super(Critic, self).__init__()
-		self.d1 = Dense(1024, activation='relu', input_shape=(input_dims, ))
+		self.d1 = Dense(1024, activation='relu')
 		self.d2 = Dense(512, activation='relu')
-		self.value = Dense(1, activation='linear')
-	
-	def __call__(self, obs):
-		x = self.d1(obs)
+		self.V = Dense(1)
+
+	def __call__(self, inp):
+		x = self.d1(inp)
 		x = self.d2(x)
-		return self.value(x)
+		return self.V(x)
 
 class Agent():
-	def __init__(self, alpha, gamma, obs_dims, n_actions):
+	def __init__(self, n_actions, alpha, gamma):
 		self.alpha = alpha
 		self.gamma = gamma
-		self.obs_dims = obs_dims
 		self.n_actions = n_actions
 
-		self.statemem = []
-		self.rewardmem = []
-		self.actionmem = []
+		self.actor = Actor(self.n_actions)
+		self.critic = Critic()
 
-		self.actor = Actor(self.obs_dims, self.n_actions)
-		self.critic = Critic(self.obs_dims)
+		self.optimizer = Adam(learning_rate=self.alpha)
+	
+	@tf.function
+	def get_action(self, obs):
+		obs = tf.reshape(obs, (1, -1))
+		probs = self.actor(obs)
+		action_probs = tfp.distributions.Categorical(probs=probs)
+		action = action_probs.sample()
+		return action
 
-		self.opt = Adam(learning_rate = self.alpha)
-
-	def push(self, s, r, a):
-		self.statemem.append(s)
-		self.rewardmem.append(r)
-		self.actionmem.append(a)
+	def get_returns(self, rewards, dones):
+		G = 0.
+		returns = []
+		for i in reversed(rewards):
+			G = i + self.gamma * G 
+			returns.append(G)
+		return np.array(returns[::-1], dtype=np.float32)*dones
 
 	@tf.function
-	def get_action(self, s):
-		s = tf.reshape(s, (-1, self.obs_dims))
-		probs = self.actor(s)
-		probs = distributions.Categorical(probs=probs)
-		a = probs.sample()
-		return a[0]
-
-	def get_returns(self):
-		returns = np.zeros(len(self.rewardmem))
-		G = 0.
-
-		for i in reversed(range(len(self.rewardmem))):
-			G = self.rewardmem[i] + self.gamma * G
-			returns[i] = G
-		return returns
-
-	def reset(self):
-		self.statemem = []
-		self.rewardmem = []
-		self.actionmem = []
-
-	def learn(self):
-		states = tf.reshape(self.statemem, (-1, self.obs_dims))
-		rewards = tf.reshape(self.rewardmem, (-1))
-		actions = tf.reshape(self.actionmem, (-1))
-		returns = self.get_returns()
-
-		
-
+	def learn(self, states, actions, rewards):
 		with tf.GradientTape(persistent=True) as tape:
-			pi = self.actor(states)
+			V = tf.squeeze(self.critic(states))
+			CriticLoss = MSE(returns, V)
+			preds = self.actor(states)
+			action_preds = tfp.distributions.Categorical(probs=preds)
+			log_probs = action_preds.log_prob(actions)
+			ActorLoss = -tf.reduce_mean(log_probs*returns)
 
-			probs = distributions.Categorical(probs = pi)
-			logProbs = probs.log_prob(actions)
+		actorGrads = tape.gradient(ActorLoss, self.actor.trainable_variables)
+		criticGrads = tape.gradient(CriticLoss, self.critic.trainable_variables)
 
-			value = tf.squeeze(self.critic(states))
-			advantage = returns - value
-			
-			critic_loss = tf.reduce_mean(advantage**2)
-			actor_loss = -tf.reduce_mean(logProbs * (advantage))
+		self.optimizer.apply_gradients(zip(actorGrads, self.actor.trainable_variables))
+		self.optimizer.apply_gradients(zip(criticGrads, self.critic.trainable_variables))
+		return CriticLoss, ActorLoss
 
-		policyGrads = tape.gradient(actor_loss, self.actor.trainable_variables)
-		criticGrads = tape.gradient(critic_loss, self.critic.trainable_variables)
-
+if __name__ == "__main__":
 	
-		
-
-		self.opt.apply_gradients(zip(policyGrads, self.actor.trainable_variables))
-		self.opt.apply_gradients(zip(criticGrads, self.critic.trainable_variables))
-		del tape
-
-		self.reset()
-
-if __name__ == '__main__':
 	env = gym.make("CartPole-v0")
-	obs_dims = env.reset().shape[0]
-	n_actions = env.action_space.n
+	
+	ALPHA = 0.0006
+	GAMMA = 0.999
+	N_ACTIONS = env.action_space.n
+	
+	agent = Agent(N_ACTIONS , ALPHA, GAMMA)
 
-	agent = Agent(0.0005, 0.99, obs_dims, n_actions)
+	wandb.init(project="A2C Cart Pole")
+	wandb.config.ALPHA = ALPHA
+	wandb.config.GAMMA = GAMMA
+	
+	n_steps = 300
 
-	avg_rewards = []
-	scores = []
-	with tqdm.trange(300) as t:
-		for episode in t:
+	reward_mem = []
+	for t in range(n_steps):
+		states = []
+		actions = []
+		rewards = []
+		next_states = []
+		dones = []
 
-			obs = env.reset()
-			done = False
-			score = 0
+		s = env.reset()
+		done = False
+		while not done:
+			a = agent.get_action(s)[0].numpy()
+			next_s, r, done, info = env.step(a)
 
-			while not done:
-				action = agent.get_action(obs).numpy()
-				obs_, reward, done, info = env.step(action)
-				agent.push(obs, reward, action)
-				obs = obs_
+			states.append(s)
+			actions.append(a)
+			rewards.append(r)
+			next_states.append(next_s)
+			dones.append(1-done)
 
-				score += reward
+			s = next_s
 
-			agent.learn()
-			scores.append(score)
+		states = np.array(states, dtype=np.float32)
+		actions = np.array(actions, dtype=np.int64)
+		rewards = np.array(rewards, dtype=np.float32)
+		next_states = np.array(next_states, dtype=np.float32)
+		dones = np.array(dones, dtype=np.float32)
+		returns = agent.get_returns(rewards, dones)
 
-			avg_rewards.append(np.mean(scores[-10:]))
-			t.set_postfix(
-				avg_reward = avg_rewards[-1]
-				)
-	plt.xlabel("Episodes")
-	plt.ylabel("Avgerage rewards over 10 episodes")
-
-	plt.plot(avg_rewards)
-	plt.show()
+		reward_mem.append(np.sum(rewards))
+		Closs, Aloss = agent.learn(states, actions, rewards)
+		
+		wandb.log({"Critic Loss":Closs, "Actor Loss":Aloss, "Average Reward":np.mean(reward_mem[-100:])})
+		print(f"Episode {t} : {np.sum(rewards)}")
